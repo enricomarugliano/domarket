cbe9f03bd078ba5e74d49b26463c8d14577b1f3f
enricomarugliano <enricomarugliano@gmail.com>
2015-11-07 15:55:05 +0000

    Added job view

--- app/src/main/java/com/example/android/domarket/JobViewActivity.java
+++ app/src/main/java/com/example/android/domarket/JobViewActivity.java
@@ -3,10 +3,14 @@ package com.example.android.domarket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class JobViewActivity extends AppCompatActivity {
 
     Job currentJob;
+    TextView jobTitle, jobDescription, suggestedBid, bid1, bid2, bid3;
+    Button placeBid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,12 @@ public class JobViewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int positionInList = intent.getIntExtra("item_position", -1);
         currentJob = JobsListFragment.jobs.get(positionInList);
+        jobTitle = (TextView) findViewById(R.id.job_view_title);
+        jobDescription = (TextView) findViewById(R.id.job_view_description);
+        suggestedBid = (TextView) findViewById(R.id.job_view_bid);
+        bid1 = (TextView) findViewById(R.id.bid_1);
+        bid2 = (TextView) findViewById(R.id.bid_2);
+        bid3 = (TextView) findViewById(R.id.bid_3);
+        placeBid = (Button) findViewById(R.id.place_bid_button);
     }
 }
