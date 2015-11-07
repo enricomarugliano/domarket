a3557851cac6ff1901fe4244af88c96b7d909a98
Dylan <dylan.stuart@vrmtechnology.co.uk>
2015-11-07 15:26:31 +0000

    Changed launcher icons

--- app/src/main/java/com/example/android/domarket/ProfileFragment.java
+++ app/src/main/java/com/example/android/domarket/ProfileFragment.java
@@ -27,7 +27,6 @@ public class ProfileFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
