01203a304d51195c9579af5feca8fb9a66f433af
enricomarugliano <enricomarugliano@gmail.com>
2015-11-07 12:35:53 +0000

    Removed slider bar to prevent crash

--- app/src/main/java/com/example/android/domarket/NewJobActivity.java
+++ app/src/main/java/com/example/android/domarket/NewJobActivity.java
@@ -8,41 +8,11 @@ import android.widget.Toast;
 
 public class NewJobActivity extends AppCompatActivity {
 
-    private SeekBar seekBar;
-    private TextView textView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_job);
-
-        textView = (TextView) findViewById(R.id.timeout_text);
-        textView.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
-
-        seekBar = (SeekBar) findViewById(R.id.timeout_slider);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress = 0;
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
-                progress = progresValue;
-                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                textView.setText("Covered: " + progress + "/" + seekBar.getMax());
-
-                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
-
-            }
-        });
 
 
     }
